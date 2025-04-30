cd7444387c3d50be48e2a57750648d659f5b3c88
Eddo <eddomora@gmail.com>
2025-04-29 23:06:11 -0400

    Creacion de clase deposito

---
+++ Primero/src/main/java/org/example/Deposito.java
@@ -0,0 +1,26 @@
+package Primero.src.main.java.org.example;
+
+import java.util.ArrayList;
+
+class Deposito<T>{
+    private ArrayList<T> dep;
+
+    public Deposito(){
+        dep = new ArrayList<T>();
+    }
+
+    public void addCosa(T cosa){
+        dep.add(cosa);
+    }
+
+    public T getCosa() {
+        if (dep.size() == 0) {
+            return null;
+        }
+        return dep.remove(0);
+    }
+
+    public int size(){
+        return dep.size();
+    }
+}
