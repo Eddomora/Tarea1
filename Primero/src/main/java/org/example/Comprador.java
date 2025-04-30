29f9c8931372ef9082962594c155c34587c45348
Vicente <vimiranda2023@udec.cl>
2025-04-30 11:25:03 -0400

    Agregación de Clase Comprador

---
+++ Primero/src/main/java/org/example/Comprador.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Comprador {
+
+    private String sonido;
+    private int vuelto;
+
+    public Comprador(Moneda m, int cualBebida, Expendedor exp) {
+        this.sonido = null;
+        this.vuelto = 0;
+
+        if (m == null) {
+            return;
+        }
+        Bebida bebi = exp.comprarBebida(m, cualBebida);
+        if (bebi != null) {
+            this.sonido = bebi.beber();
+        }
+
+        while (true) {
+            Moneda aux = exp.getVuelto();
+            if (aux == null) break;
+            vuelto += aux.getValor();
+        }
+    }
+
+    public String queBebiste() {
+        return sonido;
+    }
+    public int cuantoVuelto() {
+        return vuelto;
+    }
+}
