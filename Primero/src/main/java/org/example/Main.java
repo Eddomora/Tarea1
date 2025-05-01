637c5b9b8beccd075ca6557f598aee7bc9d512f1
Liivne <ignsoto2023@inf.udec.cl>
2025-05-01 01:24:25 -0400

    Excepciones (sin implementar)

--- Primero/src/main/java/org/example/Main.java
+++ Primero/src/main/java/org/example/Main.java
@@ -1,9 +1,7 @@
 package org.example;
 
-import java.util.ArrayList;
-
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Expendedor.PagoInsuficienteException, Expendedor.PagoIncorrectoException {
         Expendedor exp = new Expendedor(7,300);
         Moneda m = null;
         Comprador c = null;
---
+++ Primero/src/main/java/org/example/PRECIOS.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public enum PRECIOS {;
+    //en Poceso, no debería retornar su identificador, sino el precio de cada  Producto
+    public static final int COCA = 1;
+    public static final int SPRITE = 2;
+    public static final int FANTA = 3;
+    public static final int SNICKERS = 4;
+    public static final int SUPER8 = 5;
+}
