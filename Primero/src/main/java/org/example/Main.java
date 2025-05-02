7120d4f51e046b72931017d1929216eef62c0ba0
Liivne <ignsoto2023@inf.udec.cl>
2025-05-02 00:47:49 -0400

    Pruebo las excepciones en main

--- Primero/src/main/java/org/example/Main.java
+++ Primero/src/main/java/org/example/Main.java
@@ -4,14 +4,31 @@ import org.example.Excepciones.*;
 
 public class Main {
     public static void main(String[] args){
+        Expendedor exp = new Expendedor(1);
+        Moneda m = null;
+        Comprador c;
         try {
-            Expendedor exp = new Expendedor(7);
-            Moneda m = null;
-            Comprador c = null;
             c = new Comprador(m, Expendedor.COCA, PRECIOS.COCACOLA.getPrecio(), exp);
+            System.out.println(c.queCompraste() + ", tu vuelto es $" + c.cuantoVuelto());
         } catch (PagoIncorrectoException | PagoInsuficienteException | NoHayProductoException e) {
             System.err.println(e.getMessage());
         }
-        // Ahora PagoInsuficiente
+        try{
+            m = new Moneda500();
+            c = new Comprador(m, Expendedor.COCA, PRECIOS.COCACOLA.getPrecio(), exp);
+            System.out.println(c.queCompraste() + ", tu vuelto es $" + c.cuantoVuelto());
+        } catch (PagoIncorrectoException | PagoInsuficienteException | NoHayProductoException e) {
+            System.err.println(e.getMessage());
+        }
+        try{
+            m = new Moneda1500();
+            c = new Comprador(m, Expendedor.COCA, PRECIOS.COCACOLA.getPrecio(), exp);
+            System.out.println(c.queCompraste() + ", tu vuelto es $" + c.cuantoVuelto());
+
+            Comprador d = new Comprador(m, Expendedor.COCA, PRECIOS.COCACOLA.getPrecio(), exp);
+            System.out.println(d.queCompraste() + ", tu vuelto es $" + d.cuantoVuelto());
+        } catch (PagoIncorrectoException | PagoInsuficienteException | NoHayProductoException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
