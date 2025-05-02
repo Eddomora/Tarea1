98eb81e544d88477e7938f13531a835663809efa
Vicente <vimiranda2023@udec.cl>
2025-05-02 01:16:56 -0400

    Agregación de interfaz comparable

--- Primero/src/main/java/org/example/Main.java
+++ Primero/src/main/java/org/example/Main.java
@@ -2,8 +2,22 @@ package org.example;
 
 import org.example.Excepciones.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
+
+        List<Moneda> monedas = new ArrayList<>();
+        monedas.add(new Moneda500());
+        monedas.add(new Moneda100());
+        monedas.add(new Moneda1500());
+        monedas.add(new Moneda1000());
+        //Para ordernar en el main, deben usar el sort disponible en ArrayList. (pauta)
+        Collections.sort(monedas);
+
         try {
             Expendedor exp = new Expendedor(7);
             Moneda m = null;
